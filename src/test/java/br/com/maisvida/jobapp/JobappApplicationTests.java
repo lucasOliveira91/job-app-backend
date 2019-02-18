ce5982811fa2c41f9ac8ca6309f90d19b22d7ae9
Lucas <ldoliveira91@outlook.com>
2019-02-18 15:56:33 -0300

    initial commit

---
+++ src/test/java/br/com/maisvida/jobapp/JobappApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.maisvida.jobapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JobappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
