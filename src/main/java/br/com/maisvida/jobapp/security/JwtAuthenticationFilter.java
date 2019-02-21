f27a280884271a0a79ac78ca349b8b4b168b56af
Lucas <ldoliveira91@outlook.com>
2019-02-21 07:58:39 -0300

    adding filter to resolve the invalid credentials

--- src/main/java/br/com/maisvida/jobapp/security/JwtAuthenticationFilter.java
+++ src/main/java/br/com/maisvida/jobapp/security/JwtAuthenticationFilter.java
@@ -1,58 +1,83 @@
 package br.com.maisvida.jobapp.security;
 
-import br.com.maisvida.jobapp.dto.CredentialsDTO;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
 
-/**
- * Created by loliveira on 18/02/19.
- */
-public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+import br.com.maisvida.jobapp.dto.CredentialsDTO;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private AuthenticationManager authenticationManager;
+
     private JwtUtil jwtUtil;
 
     public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+        setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
         this.authenticationManager = authenticationManager;
         this.jwtUtil = jwtUtil;
     }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest req,
-                                             HttpServletResponse res) throws AuthenticationException {
+                                                HttpServletResponse res) throws AuthenticationException {
 
         try {
             CredentialsDTO creds = new ObjectMapper().readValue(req.getInputStream(), CredentialsDTO.class);
-            UsernamePasswordAuthenticationToken authToken =  new UsernamePasswordAuthenticationToken(creds.getEmail(),creds.getPassword(), new ArrayList<>());
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword(), new ArrayList<>());
             Authentication auth = authenticationManager.authenticate(authToken);
             return auth;
-        } catch (IOException e) {
-            throw  new RuntimeException(e);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
     @Override
     protected void successfulAuthentication(HttpServletRequest req,
-                                      HttpServletResponse res,
-                                      FilterChain chain,
-                                      Authentication auth) throws IOException, ServletException{
+                                            HttpServletResponse res,
+                                            FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
 
-        String userName = ((UserSS)auth.getPrincipal()).getUsername();
-        String token = jwtUtil.generateToken(userName);
-        res.setHeader("Authorization","Bearer " + token);
+        String username = ((UserSS) auth.getPrincipal()).getUsername();
+        String token = jwtUtil.generateToken(username);
+        res.addHeader("Authorization", "Bearer " + token);
 
         //Enable CORS to allow access to header 'Autorization', it's very important!
         res.addHeader("access-control-expose-headers", "Authorization");
+    }
+
+    private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+        @Override
+        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+                throws IOException, ServletException {
+            response.setStatus(401);
+            response.setContentType("application/json");
+            response.getWriter().append(json());
+        }
+
+        //Force return http status 401 when invalid credentials
+        private String json() {
+            long date = new Date().getTime();
+            return "{\"timestamp\": " + date + ", "
+                    + "\"status\": 401, "
+                    + "\"error\": \"Not Authentication\", "
+                    + "\"message\": \"Email or password invalid.\", "
+                    + "\"path\": \"/login\"}";
+        }
     }
 }
